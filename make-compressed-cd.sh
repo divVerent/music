9f4ff0ca206b4d6a32d9ca625bf04c23c898772b
Rudolf Polzer <divVerent@xonotic.org>
2018-12-29 11:08:43 -0500

    Only need 2:1, not 3:1.

--- make-compressed-cd.sh
+++ make-compressed-cd.sh
@@ -6,7 +6,7 @@ tmps=
 ./list-best-tracks.sh flac | while IFS= read -r track; do
   out=compressed/${track##*/}
   out=${out%.flac}.wav
-  sox "$track" -t wav -e floating-point -b 32 "${out}" compand 0.3,1 6:-70,-60,-20 -15 -90 0.2
+  sox "$track" -t wav -e floating-point -b 32 "${out}" compand 0.3,1 6:-70,-60,-30 -15 -90 0.2
 done
 normalize-audio compressed/*.wav
 for x in compressed/*.wav; do
